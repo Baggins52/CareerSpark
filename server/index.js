28ac95e9a5524fada65284af648074a123dc426c
Kevin Gin <kevinagin@gmail.com>
2017-02-06 19:15:37 -0800

    (cleanup) server code cleanup

--- server/index.js
+++ server/index.js
@@ -1,33 +1,24 @@
 var express = require('express');
 var fileUpload = require('express-fileupload');
 var bodyParser = require('body-parser');
-var app = express();
-// import two routers
 var userRouter = require('./routers/users.js');
 var connectionRouter = require('./routers/connections.js');
-
+var app = express();
 
 app.use(fileUpload());
 app.use(bodyParser.json());
 
-// Serves up main page
 app.use('/', express.static(__dirname + '/../client/dist'));
-
-// Serves up profile images
-app.use('/images', express.static(__dirname + '/../server/assets'))
-  // GET http://localhost:3000/images/1.jpg ==> returns image for user 1
-
-// URL ending for Users
 app.use('/api/users', userRouter);
-  // GET http://localhost:3000/api/users  ==> returns all users//
-  // GET http://localhost:3000/api/users/1  ==> returns data for user 1 (user, and connections info)
-  // POST http://localhost:3000/api/users  ==> posts new user to database, saves image to storage
-
-//URL ending for Connetions
 app.use('/api/connections', connectionRouter);
-  // GET http://localhost:3000/api/connections  ==> returns all connections (both pending and not pending)
-  // GET http://localhost:3000/api/connections/1  ==> accepts connection with ID of 1 (changes pending to false)
 
 app.listen(3000, function() {
   console.log('listening on port 3000');
 });
+
+
+  // GET http://localhost:3000/api/users  ==> returns all users
+  // GET http://localhost:3000/api/users/1  ==> returns user/connections data for user 1. Data shaped like state in client/src/app.jsx.
+  // POST http://localhost:3000/api/users  ==> posts new user. Data should match Users table in db/index.js
+  // GET http://localhost:3000/api/connections  ==> returns all connections (both pending and not pending)
+  // POST http://localhost:3000/api/connections  ==> add or accept connection. Data should include sourceUserId and targetUserId.
--- server/routers/users.js
+++ server/routers/users.js
@@ -2,49 +2,23 @@ var express = require('express');
 var router = express.Router();
 var db = require('../../db/controllers/Controller.js');
 
-// when URL ending is: .../api/users
+
 router.route('/')
-  // returns array of all users
   .get(function(req, res) {
      db.findAllUsers(function(data) {
      	res.send(data);
      })
   })
-  // posts new user. Returns user
   .post(function(req, res) {
-    // The request body should look like a user.
-    // Here's an example of a request body:
-    // {
-    //   "
-    //   "username": "RouteTesting",
-    //   "password": "unencrypted"
-    //   "phone": 8675309,
-    //   "tagline": "wowimcool",
-    //   "firstName": "michael",
-    //   "lastName": "comes",
-    //   "location": "SF",
-    //   "field": "engineering",
-    //   "company": "HR",
-    //   "imageUrl": "cloudinary.com/something"
-    // }
-
     var user = req.body;
-    // post user to db
     db.addUser(user, function(result) {
       res.send(result);
     });
   });
 
 // when URL ending is /api/users/<id>
+// returns all user data needed to set state in client/src/components/app.jsx
 router.route('/:id')
-  // returns data on user by ID. *TO DO: Return error if no user found.
-  // returns: {
-    // user: {...},
-    // pendingConnectionsIncoming: [..],
-    // pendingConnectionsOutgoing: [..],
-    // acceptedConnections: [..],
-    // suggestedConnections: [..],
-  // }
   .get(function(req, res) {
     var userId = req.url.slice(1);
     // first get our user
@@ -110,10 +84,7 @@ router.route('/:id')
     })
   })
 
-
-
-
-  // deletes user by ID *To Do: Return error if no user found.
+  // deletes user by ID
   .delete(function(req,res) {
     var userId = req.url.slice(1);
     db.deleteUser(userId, function(result) {
